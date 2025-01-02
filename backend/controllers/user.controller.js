df62381e77765171fa84ea9397b25d7c8f3a6857
Demetrius Lewis <DemetriusLewis2012@gmail.com>
2025-01-01 20:39:36 -0500

    feat: Users get suggestions for other users they should follow

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -63,7 +63,35 @@ const followUnfollowUser = async (req, res) => {
     }
 }
 
+const getSuggested = async (req, res) => {
+    try {
+        const userId = req.user._id;
+
+        const usersFollowedByMe = await User.findById(userId).select("following");
+
+        const users = await User.aggregate([
+            {
+                $match: {
+                    _id: {$ne: userId}
+        
+                }
+            },
+            {$sample: {size: 10}}
+        ]);
+
+        const filteredUsers = users.filter(user => !usersFollowedByMe.following.includes(user._id))
+        const suggestedUsers = filteredUsers.slice(0, 4);
+        
+        suggestedUsers.forEach(user => user.password = null)
+        res.status(200).json(suggestedUsers);
+    } catch (error) {
+        console.error('Error in getSuggestedUsers', error.message);
+        res.status(500).json({ error: error.message });
+    }
+}
+
 export {
     getUserProfile,
-    followUnfollowUser
+    followUnfollowUser,
+    getSuggested
 }
--- backend/routes/users.routes.js
+++ backend/routes/users.routes.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import { protectedRoute } from '../middleware/protectedRoute.js';
-import { getUserProfile, followUnfollowUser } from '../controllers/user.controller.js';
+import { getUserProfile, followUnfollowUser, getSuggested } from '../controllers/user.controller.js';
 
 const router = express.Router();
 
 router.get('/profile/:username', protectedRoute, getUserProfile);
-// router.get('/suggested', protectedRoute, getSuggested);
+router.get('/suggested', protectedRoute, getSuggested);
 router.post('/follow/:id', protectedRoute, followUnfollowUser);
 // router.post('/update', protectedRoute, updateUserProfile);
 
